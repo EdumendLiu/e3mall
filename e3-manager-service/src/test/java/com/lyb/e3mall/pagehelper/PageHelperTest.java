a65ddb69cf72aba65604e66770553deb7a4227ca
liuyubin <18732106176@163.com>
2020-12-31 10:07:20 +0800

    查询问题提交

---
+++ e3-manager-service/src/test/java/com/lyb/e3mall/pagehelper/PageHelperTest.java
@@ -0,0 +1,33 @@
+package com.lyb.e3mall.pagehelper;
+
+import java.util.List;
+
+import com.lyb.e3mall.dao.TbItemDao;
+import com.lyb.e3mall.entity.TbItem;
+import com.lyb.e3mall.entity.TbItemExample;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+public class PageHelperTest {
+
+	@Test
+	public void testPageHelper() throws Exception {
+		//初始化spring容器
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-dao.xml");
+		//从容器中获得Mapper代理对象
+		TbItemDao itemMapper = applicationContext.getBean(TbItemDao.class);
+		//执行sql语句之前设置分页信息使用PageHelper的startPage方法。
+		PageHelper.startPage(1, 10);
+		//执行查询
+		TbItem example = new TbItem();
+		List<TbItem> list = itemMapper.queryAll(example);
+		//取分页信息，PageInfo。1、总记录数2、总页数 。当前页码
+		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+		System.out.println(pageInfo.getTotal());
+		System.out.println(pageInfo.getPages());
+		System.out.println(list.size());
+	}
+}
